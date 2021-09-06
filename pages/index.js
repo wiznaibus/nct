53e00e9a28299c268eb46a60d2f7636b91090d08
wiznaibus <wiznaibuswaltz@gmail.com>
2021-09-06 14:54:58 -0600

    fix imports

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Link from 'next/link'
-import Image from 'next/image'
+import Head from 'next/head'
 
 function Home() {
     return (
@@ -16,7 +16,7 @@ function Home() {
             </Head>
             <div className="container px-3 mx-auto my-48">
                 <div className="mx-auto w-full md:w-2/3 xl:w-1/2 flex flex-nowrap items-center gap-x-1">
-                    <p className="text-4xl md:text-5xl pb-5">There's nothing here yet. Visit the <Link href="/discography"><a className="text-nct127 underline hover:text-gray-700">Discography</a></Link> page?</p>
+                    <p className="text-4xl lg:text-5xl pb-5">The home page is still under construction. Visit the <Link href="/discography"><a className="text-nct127 underline hover:text-gray-700">Discography</a></Link> page?</p>
                     <img className="h-24 w-24 p-px border border-black object-right" src="/images/haechanpeek.png" />
                 </div>
             </div>
